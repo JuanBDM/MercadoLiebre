275b598347298ffffff33e91cf795e540061c804
JuanBDM <88855781+JuanBDM@users.noreply.github.com>
2021-09-21 00:45:46 -0300

    Update app.js
    update app.js

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, ()=>{ 
-    console.log("El servidor corriendo en: http://localhost:3000/");
-});
 
 app.get("/", (req, res)=> {
     res.sendFile(path.join(__dirname, "./views/home.html"))
@@ -18,3 +15,7 @@ app.get("/login", (req, res)=> {
 app.get("/register", (req, res)=> {
     res.sendFile(path.join(__dirname, "./views/register.html"))
 });
+
+app.listen (process.env.PORT || 3000, function (){
+    console.log("servidor corriendo en el puerto 3000");
+}); 
