5d0b296c2fe3e7f0e24327011a91000f97494389
JuanBDM <88855781+JuanBDM@users.noreply.github.com>
2021-09-21 01:05:14 -0300

    Update app.js
    commit  numero mil

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+app.use(express.static("public"));
+    const PORT = process.env.PORT || 3000;
+
+app.listen (process.env.PORT || 3000, function () {
+    console.log("El servidor estsa corriendo en http://localhost:3000/")
+})
 
 app.get("/", (req, res)=> {
-    res.sendFile(path.join(dirname, "./views/home.html"))
+    res.sendFile(path.join(__dirname, "./views/home.html"))
 });
 
 app.get("/login", (req, res)=> {
-    res.sendFile(path.join(dirname, "./views/login.html"))
+    res.sendFile(path.join(__dirname, "./views/login.html"))
 });
 
 app.get("/register", (req, res)=> {
     res.sendFile(path.join(__dirname, "./views/register.html"))
-});
-
-app.listen (process.env.PORT || 3000, function (){
-    console.log("servidor corriendo en el puerto 3000");
 });
